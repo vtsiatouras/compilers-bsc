d5987e94b5d9974f55e73141e000588ca5bb508a
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2017-11-03 20:28:50 +0200

    Added line scanner to read user input

--- project_1/part_1/src/Main.java
+++ project_1/part_1/src/Main.java
@@ -1,6 +1,16 @@
-public class Main {
+import java.io.*;
+import java.util.Scanner;
 
+public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Parser parser = new Parser();
+        Scanner scanner = new Scanner(System.in);
+        String input;
+        // Read lines
+        // TODO add exception to handle ctrl+D to terminate gracefully
+        while ((input= scanner.nextLine()) != null) {
+            System.out.println(input);
+//            parser.parser(input);
+        }
     }
 }

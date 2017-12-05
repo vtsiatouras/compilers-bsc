aaa4001e4ca27c2111bd794e8e29a01efe71121e
Vangelis Tsiatouras <vangelis.tsiatouras@gmail.com>
2017-12-05 23:31:59 +0200

    Now can parse multiple files in one execution

--- project_2/src/Main.java
+++ project_2/src/Main.java
@@ -4,38 +4,40 @@ import java.io.*;
 
 class Main {
     public static void main(String[] args) {
-        // todo na to kanw na pairnei polla arxeia
-        if (args.length != 1) {
+        if (args.length == 0) {
             System.err.println("Usage: java Main [file1] [file2] ... [fileN]");
             System.exit(1);
         }
         FileInputStream fis = null;
-        try {
-            fis = new FileInputStream(args[0]);
-            MiniJavaParser parser = new MiniJavaParser(fis);
-            SymbolTable symbolTable = new SymbolTable();
-            FirstVisitor first_visit = new FirstVisitor();
-            Goal root = parser.Goal();
-            try {
-                root.accept(first_visit, symbolTable);
-                symbolTable.PrintSymbolTable();
-            }
-            catch(Exception ex) {
-                System.err.println(ex.getMessage());
-            }
-            finally {
-                symbolTable = null;
-            }
-        } catch (ParseException ex) {
-            System.out.println(ex.getMessage());
-        } catch (FileNotFoundException ex) {
-            System.err.println(ex.getMessage());
-        } finally {
+        for (int i = 0; i < args.length; i++) {
 
             try {
-                if (fis != null) fis.close();
-            } catch (IOException ex) {
+                fis = new FileInputStream(args[i]);
+                MiniJavaParser parser = new MiniJavaParser(fis);
+                System.out.println("Parsing '"+args[i]+"'");
+                SymbolTable symbolTable = new SymbolTable();
+                FirstVisitor firstVisitor = new FirstVisitor();
+                SecondVisitor secondVisitor = new SecondVisitor();
+                Goal root = parser.Goal();
+                try {
+                    root.accept(firstVisitor, symbolTable);
+                    symbolTable.PrintSymbolTable();
+//                    root.accept(secondVisitor, symbolTable);
+                } catch (Exception ex) {
+                    System.err.println(ex.getMessage());
+                } finally {
+                    symbolTable = null;
+                }
+            } catch (ParseException ex) {
+                System.out.println(ex.getMessage());
+            } catch (FileNotFoundException ex) {
                 System.err.println(ex.getMessage());
+            } finally {
+                try {
+                    if (fis != null) fis.close();
+                } catch (IOException ex) {
+                    System.err.println(ex.getMessage());
+                }
             }
         }
     }
